1b7f036d8b769c0c9374096a38f5bdcec2023527
Shakti Gupta <74551736+skcse123@users.noreply.github.com>
2021-08-30 13:07:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './fliqaindia.png';
+import './App.css';
+import Header from "./Mycomponents/Header";
+function App() {
+  return (
+    <>
+    <div className="App">
+    <Header className="App">
+      <img src={"fliqaindia.png"} className="fliqaindia.png" alt="fliqaindia.png" />
+    </Header>
+    </div>
+    </>
+  );
+}
+
+export default App;
